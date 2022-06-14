4019896e039a55aeb66c25caeb5fcd5a9593149a
Abhinav Khetarpal <khetarpalabhinav@gmail.com>
2022-06-14 16:17:37 -0400

    Added another solution the problem without the use of map method

--- JavaScript/splitAndMerge.js
+++ JavaScript/splitAndMerge.js
@@ -1,3 +1,14 @@
 function splitAndMerge(string, separator) {
     return string.split(" ").map(x => x.split("").join(separator)).join(" ")
   }
+
+//without map
+
+function splitAndMerge(string,separator) {
+  let words = string.split(" ")
+  let newArr = []
+  for (let i = 0; i < words.length; i++){
+    newArr.push(words[i].split("").join(separator))
+  }
+  return newArr.join(" ")
+}
