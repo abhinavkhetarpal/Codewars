b77c2860acf312a972da7182c17ad7d97b8a76c4
Abhinav Khetarpal <khetarpalabhinav@gmail.com>
2022-08-18 13:14:45 -0400

    Debugged the rooms object

---
+++ JavaScript/rooms.js
@@ -0,0 +1,35 @@
+// Find all of the errors in the following rooms object and debug them
+
+// var rooms = {
+//   first: {
+//     description: 'This is the first room',
+//     items: {
+//       chair: 'The old chair looks comfortable',
+//       lamp: 'This lamp looks ancient'
+//   },
+//   second: {
+//     description: 'This is the second room',
+//     items: {
+//       couch: 'This couch looks like it would hurt your back',
+//       table: 'On the table there is an unopened bottle of water'
+//       }
+//     }
+//   }
+// }
+
+var rooms = {
+    first: {
+      description: 'This is the first room',
+      items: {
+        chair: 'The old chair looks comfortable',
+        lamp: 'This lamp looks ancient'
+      }
+    },
+    second: {
+      description: 'This is the second room',
+      items: {
+        couch: 'This couch looks like it would hurt your back',
+        table: 'On the table there is an unopened bottle of water'
+      }
+    }
+  }
