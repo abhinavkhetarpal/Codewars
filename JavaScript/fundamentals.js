3424b29ce1c3a1e2ddc5a9f85ee50cfcd44a1a86
Abhinav Khetarpal <khetarpalabhinav@gmail.com>
2022-08-22 23:11:10 -0400

    Made multiple functions that will return the sum, difference, modulus, product, quotient, and the exponent respectively

---
+++ JavaScript/fundamentals.js
@@ -0,0 +1,34 @@
+// Function Declarations
+
+function add(a,b){
+    return a+b
+}
+
+function divide(a,b){
+    return a/b
+}
+
+function multiply(a,b){
+    return a*b
+}
+
+function mod(a,b){
+    return a%b
+}
+   
+function exponent(a,b){
+    return a**b
+}
+    
+function subt(a,b){
+    return a-b
+}
+
+// Arrow functions
+
+const add = (a, b) => a + b;
+const subt = (a, b) => a - b;
+const divide = (a, b) => a / b;
+const multiply = (a, b) => a * b;
+const mod = (a, b) => a % b;
+const exponent = (a, b) => a ** b;
