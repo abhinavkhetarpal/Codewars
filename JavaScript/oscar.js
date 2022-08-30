d98081b3f333ce5f7f065ae486251e1fa2cb0e5f
Abhinav Khetarpal <khetarpalabhinav@gmail.com>
2022-08-30 19:50:31 -0400

    This function describes Leo

---
+++ JavaScript/oscar.js
@@ -0,0 +1,12 @@
+function leo(oscar){
+    switch(true){
+        case oscar == 88:
+          return "Leo finally won the oscar! Leo is happy"
+        case oscar == 86:
+          return "Not even for Wolf of wallstreet?!"
+        case oscar < 88:
+          return "When will you give Leo an Oscar?"
+        case oscar > 88:
+          return "Leo got one already!"
+    }
+  }
