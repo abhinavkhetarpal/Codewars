23e8dbf2ebc41adef02012ae153f85fa3f9846e1
Abhinav Khetarpal <khetarpalabhinav@gmail.com>
2022-11-03 23:44:14 -0400

    Updated

---
+++ JavaScript/GetSum.js
@@ -0,0 +1,33 @@
+function getSum( a,b )
+{
+   if (a === b) return a
+  let sum = 0
+  if (a < b){
+      while (a <= b){
+        sum += a
+        a++
+      }
+    return sum
+  }
+  if (a > b){
+    while (a >= b){
+      sum += a
+      a--
+    }
+    return sum
+  }
+}
+
+// Refactored
+
+function GetSum( a,b )
+{
+  let sum = 0;
+  
+  if(a < b)
+    while(a <= b) sum += a++;
+  else
+    while(a >= b) sum += a--;
+      
+  return sum;
+}
