759cdb4d6a3480fc03759ae2f2f9356d8499d3ca
Abhinav Khetarpal <khetarpalabhinav@gmail.com>
2022-12-01 09:29:24 -0500

    This function returns the total sum of internal angles (in degrees) of any n-sided simple polygon. N is greater than 2

---
+++ JavaScript/angle.js
@@ -0,0 +1,10 @@
+/*
+Instructions
+
+Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+
+*/
+
+function angle(n) {
+    return 180 * (n - 2)
+  }
