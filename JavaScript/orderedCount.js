c98136d0863e061e9164e3459f778ec48464b0dd
Abhinav Khetarpal <khetarpalabhinav@gmail.com>
2023-01-15 23:44:05 -0500

    Updated

--- JavaScript/orderedCount.js
+++ JavaScript/orderedCount.js
@@ -10,4 +10,32 @@ orderedCount("abracadabra") == [['a', 5], ['b', 2], ['r', 2], ['c', 1], ['d', 1]
 
 */
 
-const orderedCount = s => Array.from(s.split('').reduce((m, k) => m.set(k, m.has(k) ? m.get(k) + 1 : 1), new Map()));
+/*
+Parameters: string
+Returns: array
+Examples: orderedCount("abracadabra") -> [['a',5],['b',2]...]
+Pseudocode: 
+  // convert string to array
+  // loop through the array
+  // add each character of the string to a Map
+  // if the character already exists within the Map, increase its count
+  // return the properties of the Map in array format
+*/
+
+const orderedCount = function (text) {
+    const arr = text.split("")
+    const res = arr.reduce((m,x) => {
+      if (!m.has(x)){
+        m.set(x,0) 
+      }
+      m.set(x,m.get(x) + 1)
+      return m
+    } ,new Map())
+    return Array.from(res)
+  }
+
+// Refactored using ternary operator
+
+const orderedCount = function (text) {
+    return Array.from(text.split("").reduce((m,x) => m.set(x, m.has(x) ? m.get(x) + 1 : 1),new Map()))
+  }
