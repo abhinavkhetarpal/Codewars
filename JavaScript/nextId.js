5c0b2a4b65db2b822cfed29d43637dad72fd5297
Abhinav Khetarpal <khetarpalabhinav@gmail.com>
2023-03-25 18:53:27 -0400

    Added alternative solution

--- JavaScript/nextId.js
+++ JavaScript/nextId.js
@@ -54,3 +54,12 @@ function nextId(ids){
         if (!used.has(i)) return i
     }
 }
+
+// Alternative Solution
+
+function nextId(ids){
+    for (let i = 0; i < ids.length; i++){
+        if (ids.indexOf(i) == -1) return i
+    }
+    return ids.length
+}
